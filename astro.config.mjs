5db11fcc1dcd728d32df7a3b73114f6a9be527a3
Anar <anar.sol.dev@gmail.com>
2026-02-03 23:03:00 +0100

    add github action config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
-  site: "https://anar-sol.github.io/astro-link-in-bio/",
+  site: "https://anar-sol.github.io/",
+  base: "astro-link-in-bio/"
 });
